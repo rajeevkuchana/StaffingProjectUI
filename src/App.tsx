c300a5ecae26a3733de79e0bd0be779a9727f9bc
khushbumaurya101 <khushbumaurya101@gmail.com>
2024-08-14 23:40:15 +0530

    menu highlight issue

--- src/App.tsx
+++ src/App.tsx
@@ -42,7 +42,7 @@ const App = () => {
             <Route path='/client/profile/:jobCategory/:jobProfileSub/:id/result-list' element={<Profiles />} />
             <Route path='/client/profile-detail/:jobCategory/:id' element={<SearchUser />} />
 
-            <Route path='/client/selected' element={<Selected />} />
+            <Route path='/client/shortlist' element={<Selected />} />
             <Route path='/interviwer/search' element={<InterviwerSearch />} />
             <Route path='/interviwer/create' element={<InterviwerUser />} />
             <Route path='/admin/user' element={<AddUsers />} />
