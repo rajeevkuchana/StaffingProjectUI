9d5da07f350ba21a1dc5c594027f9e19bd7729ac
Rajeev <rajeevjskuchana@gmail.com>
2024-08-25 14:44:37 +0530

    updated the IP of the server for the Proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:8090',
+      target: 'http://43.205.97.239:8090',
       changeOrigin: true,
     })
   );
