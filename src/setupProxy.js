8ddb5841c241605217594b50ca05bb70886c9d14
Rajeev <rajeevjskuchana@gmail.com>
2024-10-31 23:46:02 +0530

    added IP instead of localhost

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:8090',
+      target: 'http://3.81.66.16:8090',
       changeOrigin: true,
     })
   );
