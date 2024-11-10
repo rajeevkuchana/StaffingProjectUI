8aba48103b9cd11c0788bc708c18ed002ce1aa30
Rajeev <rajeevjskuchana@gmail.com>
2024-11-10 23:59:25 +0530

    portal url changes

--- src/Utils/Const.ts
+++ src/Utils/Const.ts
@@ -5,7 +5,7 @@ export const userRole = {
     recruiter: "Recruiter"
 }
 
-export const apiBaseAddress = 'http://3.81.66.16:8090';
+export const apiBaseAddress = 'https://portal.quantlytixsolutions.com/backend';
 
 export const Roles = [
     { name: userRole.client, code: userRole.client, },
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://3.81.66.16:8090',
+      target: 'https://portal.quantlytixsolutions.com/backend',
       changeOrigin: true,
     })
   );
