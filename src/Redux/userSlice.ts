632d38f467baaee370059261d67d394820c08400
khushbumaurya101 <khushbum.wa@gmail.com>
2024-12-29 23:50:06 +0530

    user added in both place

--- src/Redux/userSlice.ts
+++ src/Redux/userSlice.ts
@@ -1,6 +1,7 @@
 import { PayloadAction, createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { apiBaseAddress } from '../Utils/Const';
 import axios from 'axios';
+import { ISignUpDetails } from '../Types/AuthType';
 
 // Define a type for the slice state
 interface UserState {
@@ -30,13 +31,69 @@ export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
 
 export const createUsers = createAsyncThunk('users/addUser', async (user: any) => {
   const response = await axios.post<[]>(`${apiBaseAddress}/user/add`, user);
-  return response.data;
+  const data = {
+    "attributes": {
+      "locale": "",
+      "company": user.company
+    },
+    "requiredActions": [],
+    "emailVerified": false,
+    "username": user.username,
+    "email": user.email,
+    "groups": [],
+    "enabled": true,
+    "credentials": [
+      {
+        "type": "password",
+        "value": user.password,
+        "temporary": false
+      }
+    ]
+  }
+
+  const _headers = {
+    'Content-Type': 'application/json',
+    "authorization": `Bearer ${localStorage.getItem("keycloak-token")}`
+  }
+  const _response = await axios.post(`https://portal.quantlytixsolutions.com/auth/admin/realms/quantlytix/users`, data, {
+    headers: _headers
+  });
+  console.log(_response.headers);
+  const userId = _response.headers.location.split("/")[_response.headers.location.split("/").length - 1]
+  const roles = await axios.get(`https://portal.quantlytixsolutions.com/auth/admin/realms/quantlytix/roles?first=0&max=21`, {
+    headers: _headers
+  })
+  const roleId = roles.data.find(x => x.name === user.role)
+  const roleData = [
+    {
+      "id": roleId.id,
+      "name": user.role,
+      "description": "",
+      "composite": false,
+      "clientRole": false
+      // "containerId": "6413762d-9d7f-4b17-9f33-e89d7295e912"
+    }
+  ]
+  const _role = await axios.post(`https://portal.quantlytixsolutions.com/auth/admin/realms/quantlytix/users/${userId}/role-mappings/realm`, roleData, {
+    headers: _headers
+  });
 });
 
 export const deleteUsers = createAsyncThunk('users/deleteUsers', async (id: any) => {
   const response = await axios.delete<[]>(`${apiBaseAddress}/user/delete/${id}`);
   return response.data;
 });
+
+export const keyclockAddUser = createAsyncThunk('users/keyclockAddUser', async (user: ISignUpDetails) => {
+
+  try {
+
+    // return response.data
+  } catch (err) {
+    return err
+  }
+});
+
 
 const userSlice = createSlice({
   name: 'client',
@@ -67,7 +124,7 @@ const userSlice = createSlice({
       .addCase(createUsers.fulfilled, (state: UserState, action) => {
         state.createStatus = 'succeeded';
       })
-     
+
   },
 });
 
